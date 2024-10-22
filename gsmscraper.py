f8e4dc440e678947c27881455802983f59f11a91
Nilson Herbert <72417934+nilsinhojrx@users.noreply.github.com>
2024-10-22 14:29:32 -0300

    Update gsmscraper.py

--- gsmscraper.py
+++ gsmscraper.py
@@ -16,7 +16,7 @@ class GSMSpider(scrapy.Spider):
         'AUTOTHROTTLE_ENABLED': True,
         'AUTOTHROTTLE_MAX_DELAY': 10,
         'AUTOTHROTTLE_START_DELAY': 2,
-        'AUTOTHROTTLE_TARGET_CONCURRENCY': 500,
+        'AUTOTHROTTLE_TARGET_CONCURRENCY': 20,
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
     }
 
